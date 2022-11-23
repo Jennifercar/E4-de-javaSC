c576034103852396a0ab117063d25fae849b40e1
Jennifercar <jennifercarrasco.lp@gmail.com>
2022-11-23 03:38:25 -0300

    (solucionar fech)

--- index.js
+++ index.js
@@ -6,17 +6,12 @@ const input_buton = document.getElementById('input-buton')
 
 let pokemones = JSON.parse(localStorage.getItem("pokemones")) || [];
 
-const saveLocalStorage = (pokeList) => {
-  localStorage.setItem("pokemones", JSON.stringify(pokeList));
+const saveLocalStorage = () => {
+  localStorage.setItem("pokemones", JSON.stringify(pokemones));
 };
 
 const baseURL = "https://pokeapi.co/api/v2/pokemon/";
 
-let isFetching = false;
-
-const nextURL = {
-  next: null,
-};
 
 const renderPokemon = (pokemon) => {
     const { id, name, sprites, height, weight, types } = pokemon;
@@ -40,47 +35,38 @@ const renderPokemon = (pokemon) => {
     `;
   };
 
-  const renderPokemonList = (pokeList) => {
-    conteiner.innerHTML = pokeList.map((pokemon) => renderPokemon(pokemon)).join("");
+  const renderPokemonList = (pokemon) => {
+    pokemones=pokemon;
+    saveLocalStorage();
+    conteiner.innerHTML = renderPokemon(pokemones);
   };
 
   const searchPoke = async (e) => {
     e.preventDefault();
   
-    const searchedPoke = input_buton.value.trim();
+  const searchedPoke = input_number.value;
   
     if (searchedPoke === "") {
       alert("Por favor ingrese un numero");
       return;
     }
-     }
-    const fetchPokemons = async () => {
+     
+const fetchPokemons = async () => {
       const res = await fetch(`${baseURL}`);
       const data = await res.json();
     
       return data;
     };
+    
+    renderPokemonList(fetchPokemons)
+    
+};
 
-    function init() {
-      window.addEventListener("DOMContentLoaded", async () => {
-        let { next, results } = await fetchPokemons();
-    
-        nextURL.next = next;
-    
-        const URLS = results.map((pokemon) => pokemon.url);
-    
-        const InfoPokemones = await Promise.all(
-          URLS.map(async (url) => {
-            const nextPokemons = await fetch(url);
-            return await nextPokemons.json();
-          })
-        );
-    
-        renderPokemonList(InfoPokemones);
-      });
- }
 
-  
-  
-  init();
-  
+ 
+ const init = () => {
+   form.addEventListener("submit", searchPoke);
+   renderPokemonList(pokemones);
+ };
+ 
+ init();
